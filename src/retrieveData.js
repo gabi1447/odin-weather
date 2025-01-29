36c020e6d79977c3b7e3fd9ae1cc0ea038f20b6b
Gabriel <gabrielpachecotec@gmail.com>
2025-01-29 19:43:20 +0000

    Select needed fields from weather API response to display to the user

--- src/retrieveData.js
+++ src/retrieveData.js
@@ -12,6 +12,17 @@ export async function retrieveWeatherData(location) {
     }
 }
 
+export function returnMainFieldsFromWeatherDataResponse(response) {
+    return {
+        address: response["resolvedAddress"],
+        temp: response["currentConditions"]["temp"],
+        dateTime: response["currentConditions"]["datetime"],
+        conditions: response["days"][0]["conditions"],
+        description: response["days"][0]["description"],
+        icon: response["days"][0]["icon"],
+    };
+}
+
 export async function retrieveGif(valueToSearch) {
     const url = `https://api.giphy.com/v1/gifs/translate?api_key=96hwjEWRpXYc3YVHw3G3YbSTK3fslPO3&s=${valueToSearch}`;
 
