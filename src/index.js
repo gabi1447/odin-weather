176ca7e6190a18634852e1c89d00f80001da0cf5
Gabriel <gabrielpachecotec@gmail.com>
2025-01-30 13:24:58 +0000

    Remove unused imports

--- src/index.js
+++ src/index.js
@@ -2,18 +2,6 @@ import "./styles.css";
 import "./styles-dynamic-elements.css";
 import "./styles-weather-card.css";
 
-import {
-    celsiusToFahrenheit,
-    clearContentFromElement,
-    FahrenheitToCelsius,
-} from "./generalPurpose";
-
-import {
-    retrieveWeatherData,
-    retrieveGif,
-    returnMainFieldsFromWeatherDataResponse,
-} from "./retrieveData";
-
 import { addSearchComponent } from "./dynamicElements";
 
 import { setupDynamicEventListeners } from "./eventListeners";
